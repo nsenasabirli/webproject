5a59734509f1010138b227a8e5f549950df6d767
elifnazkadayifci <elifnazkadayifci@gmail.com>
2024-06-07 22:41:03 +0300

    update

--- GameDetails.php
+++ GameDetails.php
@@ -135,7 +135,7 @@
         <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
       </symbol>
     </svg>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <nav class="navbar navbar-expand-md fixed-top">
   <div class="container-fluid">
   	<img src ="wow.png" width="100px">
     <a class="navbar-brand" href="#"><b><i>WOW Books</b></i></a>
@@ -235,8 +235,9 @@
                 echo '
                 <div style="background-color: beige;" class="card mb-3">
                     <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="'.$row["image_url"].'" class="img-fluid rounded-start" alt="'.$row["names"].'">                        </div>
+                    <div class="col-md-4">
+                    <img src="'.$row["coverImg"].'" class="img-fluid rounded-start" alt="'.$row["title"].'" style="display: block; margin: auto;">
+                     </div>
                         <div class="col-md-8">
                           <div style="border: 1px solid black; padding: 20px;">
                             <div class="card-body">
